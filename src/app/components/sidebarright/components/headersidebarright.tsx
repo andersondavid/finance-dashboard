d5bfbe3c284ee8f0fe0aed152237ddd2b5d52639
Anderson David <andersondavidfreitas@gmail.com>
2023-06-14 19:44:42 -0300

    fix(headersidebarright): fix paddings and margins

--- src/app/components/sidebarright/components/headersidebarright.tsx
+++ src/app/components/sidebarright/components/headersidebarright.tsx
@@ -3,7 +3,7 @@ import { LuBell, LuWallet } from "react-icons/lu";
 
 export default function HeaderSideBarRight() {
 	return (
-		<div className="w-min flex p-4 pr-0 ml-auto">
+		<div className="w-min flex m-8 mr-0 mb-12 ml-auto">
 			<div className="p-2 pl-3 h-11 w-28 border-2 border-slate-800 rounded-full">
 				<p className="inline text-gray-400 m-0">
 					<LuWallet className="h-5 w-5 text-gray-400 mr-2 mb-1 inline" />
