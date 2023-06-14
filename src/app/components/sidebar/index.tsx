c947142a23f5ef27df2c583bfb6660ee84848530
Anderson David <andersondavidfreitas@gmail.com>
2023-06-13 23:55:24 -0300

    fix(sidebar): Fix the border line

--- src/app/components/sidebar/index.tsx
+++ src/app/components/sidebar/index.tsx
@@ -12,7 +12,7 @@ import {
 
 export default function SideBar() {
 	return (
-		<div className="w-min h-screen bg-slate-950 border-r-2 border-slate-900">
+		<div className="w-min h-screen bg-slate-950 border-r-2 border-slate-800">
 			<div className="p-2 pt-9">
 				<div className="h-11 w-11 bg-green-600 rounded-2xl p-3 border-4 border-gray-400"></div>
 			</div>
