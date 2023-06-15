653d23347f8e441c7e612ad3637fdb65b187571b
Anderson David <andersondavidfreitas@gmail.com>
2023-06-14 22:27:53 -0300

    fix(all mains): Remove h-screen

--- src/app/components/maincontent/index.tsx
+++ src/app/components/maincontent/index.tsx
@@ -5,7 +5,7 @@ import Transactions from "./components/transactions";
 
 export default function MainContent() {
 	return (
-		<div className="w-full h-screen bg-slate-950 p-6">
+		<div className="w-full bg-slate-950 p-6">
 			<div className="max-w-screen-xl m-auto">
 				<MainHeader />
 				<InventoryDetails />
--- src/app/components/sidebarright/index.tsx
+++ src/app/components/sidebarright/index.tsx
@@ -4,7 +4,7 @@ import CardDetails from "./components/carddetails";
 
 export default function SideBarRight() {
 	return (
-		<div className="h-screen w-min bg-slate-950 p-6 pl-0">
+		<div className="w-min bg-slate-950 p-6 pl-0">
 			<HeaderSideBarRight />
 			<CardDetails />
 		</div>
