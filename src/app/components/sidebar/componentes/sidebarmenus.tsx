dde82fd46d1e4e49ac777afd3a5526f0484669fb
Anderson David <andersondavidfreitas@gmail.com>
2023-06-14 22:57:33 -0300

    refactor(SideBar): Change colors

--- src/app/components/sidebar/componentes/sidebarmenus.tsx
+++ src/app/components/sidebar/componentes/sidebarmenus.tsx
@@ -13,50 +13,50 @@ import {
 export default function SideBarMenus() {
 	return (
 		<div className="pt-6">
-			<div className="bl-3 pl-2 pr-3 border-l-4 border-slate-600">
-				<div className="p-2 mt-2 rounded-xl hover:bg-slate-800 bg-slate-600 cursor-pointer">
-					<LuHome className="h-5 w-5 text-gray-400" />
+			<div className="bl-3 pl-2 pr-3 border-l-4 border-gray-700">
+				<div className="p-2 mt-2 rounded-xl bg-gray-700 cursor-pointer">
+					<LuHome className="h-5 w-5 text-sky-100" />
 				</div>
 			</div>
 
 			<div className="bl-3 pl-2 pr-3 border-l-4 border-transparent">
 				<div className="p-2 mt-2 rounded-xl hover:bg-slate-800 cursor-pointer">
-					<LuWallet className="h-5 w-5 text-gray-400" />
+					<LuWallet className="h-5 w-5 text-sky-100" />
 				</div>
 			</div>
 
 			<div className="bl-3 pl-2 pr-3 border-l-4 border-transparent">
 				<div className="p-2 mt-2 rounded-xl hover:bg-slate-800 cursor-pointer">
-					<LuBarChart3 className="h-5 w-5 text-gray-400" />
+					<LuBarChart3 className="h-5 w-5 text-sky-100" />
 				</div>
 			</div>
 
 			<div className="bl-3 pl-2 pr-3 border-l-4 border-transparent">
 				<div className="p-2 mt-2 rounded-xl hover:bg-slate-800 cursor-pointer">
-					<LuStore className="h-5 w-5 text-gray-400" />
+					<LuStore className="h-5 w-5 text-sky-100" />
 				</div>
 			</div>
 
 			<div className="bl-3 pl-2 pr-3 border-l-4 border-transparent">
 				<div className="p-2 mt-2 rounded-xl hover:bg-slate-800 cursor-pointer">
-					<LuShoppingBag className="h-5 w-5 text-gray-400" />
+					<LuShoppingBag className="h-5 w-5 text-sky-100" />
 				</div>
 			</div>
 
 			<div className="bl-3 pl-2 pr-3 border-l-4 border-transparent cursor-pointer">
 				<div className="p-2 mt-2 rounded-xl hover:bg-slate-800">
-					<LuCalendarDays className="h-5 w-5 text-gray-400" />
+					<LuCalendarDays className="h-5 w-5 text-sky-100" />
 				</div>
 			</div>
 			<div className="bl-3 pl-2 pr-3 border-l-4 border-transparent cursor-pointer">
 				<div className="p-2 mt-2 rounded-xl hover:bg-slate-800">
-					<LuMessagesSquare className="h-5 w-5 text-gray-400" />
+					<LuMessagesSquare className="h-5 w-5 text-sky-100" />
 				</div>
 			</div>
 
 			<div className="bl-3 pl-2 pr-3 border-l-4 border-transparent cursor-pointer">
-				<div className="p-2 mt-12 rounded-xl hover:bg-slate-500">
-					<LuSettings className="h-5 w-5 text-gray-400" />
+				<div className="p-2 mt-12 rounded-xl hover:hover:bg-slate-800">
+					<LuSettings className="h-5 w-5 text-sky-100" />
 				</div>
 			</div>
 		</div>
--- src/app/components/sidebar/index.tsx
+++ src/app/components/sidebar/index.tsx
@@ -3,9 +3,9 @@ import SideBarMenus from "./componentes/sidebarmenus";
 
 export default function SideBar() {
 	return (
-		<div className="w-min bg-slate-950 border-r-2 border-slate-800">
+		<div className="w-min bg-gray-900 border-r-2 border-gray-800">
 			<div className="p-2 pt-9">
-				<div className="h-11 w-11 bg-green-600 rounded-2xl p-3 border-4 border-gray-400"></div>
+				<div className="h-11 w-11 bg-green-600 rounded-2xl p-3 border-4 border-sky-100"></div>
 			</div>
 			<SideBarMenus />
 		</div>
