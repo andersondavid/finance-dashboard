76c242fc4d66327bb5cf77a9f6ce065511f8c33a
Anderson David <andersondavidfreitas@gmail.com>
2023-06-14 21:18:12 -0300

    fix(cardlist): Fix cards size and responsive

--- src/app/components/maincontent/components/inventorydetails/cardslist.tsx
+++ src/app/components/maincontent/components/inventorydetails/cardslist.tsx
@@ -4,15 +4,17 @@ import { LuPlus } from "react-icons/lu";
 export default function CardsList() {
 	return (
 		<div className="flex px-6 gap-6">
-			<div className="">
-				<div className="bg-green-200 w-72 h-52 rounded-3xl"></div>
+			<div className="w-1/3 max-w-xs">
+				<div className="bg-green-200 w-full aspect-[8/5] rounded-3xl"></div>
 			</div>
-			<div className="">
-				<div className="bg-green-200 w-72 h-52 rounded-3xl"></div>
+			<div className="w-1/3 max-w-xs">
+				<div className="bg-green-200 w-full aspect-[8/5] rounded-3xl"></div>
 			</div>
-			<div className="">
-				<div className="w-72 h-52 rounded-3xl border-dashed border-slate-700 border-2 flex justify-center items-center">
-					<p className="text-lg"><LuPlus className="h-6 w-6 text-gray-200 inline mb-1" /> Add card</p>
+			<div className="w-1/3 max-w-xs">
+				<div className="w-full aspect-[8/5] rounded-3xl border-dashed border-slate-700 border-2 flex justify-center items-center">
+					<p className="text-lg">
+						<LuPlus className="h-6 w-6 text-gray-200 inline mb-1" /> Add card
+					</p>
 				</div>
 			</div>
 		</div>
