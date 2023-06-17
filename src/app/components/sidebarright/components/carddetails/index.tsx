9e59a126920dfa6b32f305b9d83e03483dc50164
Anderson David <andersondavidfreitas@gmail.com>
2023-06-16 23:14:34 -0300

    refactor(CardDetails): Change card component

--- src/app/components/sidebarright/components/carddetails/index.tsx
+++ src/app/components/sidebarright/components/carddetails/index.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import HeaderCardDetails from "./components/headercarddetails";
 import NumberCardDetails from "./components/numberscarddetails";
 import SpendingLimits from "./components/spendinglimits";
+import Card from "@/app/components/gerenic/card";
 
 export default function CardDetails() {
 	return (
 		<div className="border-2 border-gray-800 rounded-2xl w-max">
 			<HeaderCardDetails />
-			<div className="p-12 py-8">
-				<div className="bg-green-200 w-36 h-48 rounded-3xl"></div>
+			<div className="py-14 px-2">
+				<div className="rotate-90">
+					<Card />
+				</div>
 			</div>
 			<NumberCardDetails />
 			<hr className="border-t-2 border-gray-800 m-4" />
