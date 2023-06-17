cc877531d8fe8f395bef495e28df6f7ead6de190
Anderson David <andersondavidfreitas@gmail.com>
2023-06-16 23:06:07 -0300

    fix(YourBalance): Text sizes

--- src/app/components/maincontent/components/inventorydetails/components/yourbalance.tsx
+++ src/app/components/maincontent/components/inventorydetails/components/yourbalance.tsx
@@ -3,11 +3,11 @@ import { LuArrowRight } from "react-icons/lu";
 
 export default function YourBalance() {
 	return (
-		<div className="w-full h-24 flex px-6">
-			<div className="bg-gray-200 w-1/2 rounded-l-2xl p-4">
-				<p className=" text-neutral-900 text-sm">Your balance</p>
-				<h1 className="text-neutral-900 font-extrabold text-xl">$45,50</h1>
-				<p className=" text-neutral-900 text-xs">3 CARD</p>
+		<div className="w-full flex px-6">
+			<div className="bg-gray-200 w-1/2 rounded-l-2xl p-6 px-8">
+				<p className=" text-neutral-900 text-base">Your balance</p>
+				<h1 className="text-neutral-900 font-extrabold text-3xl">$45,50</h1>
+				<p className=" text-neutral-900 text-sm">3 CARD</p>
 			</div>
 			<div className="bg-green-400 w-1/2 rounded-r-2xl relative">
 				<div className="absolute bottom-4 right-4">
