bd6964366aed53d10de6ea3e6107c16d62e36395
Anderson David <andersondavidfreitas@gmail.com>
2023-06-22 23:47:21 -0300

    fix(CardStats): add margin to top of stats

--- src/app/components/maincontent/components/inventorydetails/components/cardsstats.tsx
+++ src/app/components/maincontent/components/inventorydetails/components/cardsstats.tsx
@@ -3,9 +3,9 @@ import { LuShoppingBag, LuTrendingDown, LuTrendingUp } from "react-icons/lu";
 
 export default function CardStats() {
 	return (
-		<div className="border-2 border-slate-800 rounded-2xl m-6 p-6">
+		<div className="border-2 border-slate-800 rounded-2xl m-6 p-6 pt-4">
 
-			<div className="inline-flex mr-12">
+			<div className="inline-flex mr-12 pt-2">
 				<div className="w-11 h-11 bg-teal-300 rounded-2xl p-3">
 					<LuTrendingUp className="w-5 h-5 text-slate-900" />
 				</div>
@@ -14,7 +14,7 @@ export default function CardStats() {
 					<h1 className="font-bold">$10.500,50</h1>
 				</div>
 			</div>
-			<div className="inline-flex mr-12">
+			<div className="inline-flex mr-12 pt-2">
 				<div className="w-11 h-11 bg-pink-300 rounded-2xl p-3">
 					<LuTrendingDown className="w-5 h-5 text-slate-900" />
 				</div>
@@ -23,7 +23,7 @@ export default function CardStats() {
 					<h1 className="font-bold">$4.565,75</h1>
 				</div>
 			</div>
-			<div className="inline-flex">
+			<div className="inline-flex mr-12 pt-2">
 				<div className="w-11 h-11 bg-orange-300 rounded-2xl p-3">
 					<LuShoppingBag className="w-5 h-5 text-slate-900" />
 				</div>
