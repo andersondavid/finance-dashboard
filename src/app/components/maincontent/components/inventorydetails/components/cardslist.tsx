bd58a012745c9f9fa74a3c060d817299bd289005
Anderson David <andersondavidfreitas@gmail.com>
2023-06-26 23:10:55 -0300

    fix(CardsList): Fix card size

--- src/app/components/maincontent/components/inventorydetails/components/cardslist.tsx
+++ src/app/components/maincontent/components/inventorydetails/components/cardslist.tsx
@@ -6,16 +6,16 @@ export default function CardsList() {
 	return (
 		<div className="overflow-x-scroll scrollbar-hide">
 			<div className="px-6 gap-6 flex flex-wrap w-max whitespace-nowrap">
-				<div className="w-80">
+				<div className="w-60 lg:w-80">
 					<Card />
 				</div>
-				<div className="w-80">
+				<div className="w-60 lg:w-80">
 					<Card />
 				</div>
-				<div className="w-80">
+				<div className="w-60 lg:w-80">
 					<Card />
 				</div>
-				<div className="w-80">
+				<div className="w-60 lg:w-80">
 					<div className="w-full aspect-[8/5] rounded-3xl border-dashed border-slate-700 border-2 flex justify-center items-center">
 						<p className="text-lg">
 							<LuPlus className="h-6 w-6 text-gray-200 inline mb-1" /> Add card
